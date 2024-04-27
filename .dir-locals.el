a032f74b159676a1a97af1ec989c8885a1df3170
Manuel Vázquez Acosta <mvaled@pm.me>
2024-04-27 10:05:40 +0200

    Update to Rescript 11.1

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,9 @@
-;;; Directory Local Variables
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil . ((projectile-project-compilation-cmd . "source .envrc; make compile")))
+  (js-json-mode . ((tab-width . 2)
+                    (js-indent-level . 2)))
   (json-mode . ((js-indent-level . 2)))
   (rescript-mode . ((projectile-project-compilation-cmd . "source .envrc; make compile")
                      (indent-tabs-mode . nil))))
